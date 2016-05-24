8f193ec5332f01c9f4d92dcc036c7430c945a1b4
tonydisera <tony.disera@gmail.com>
2016-05-24 12:04:41 -0600

    Default heat map to evaluate "overlaps" field.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var heatmap = null;
 var giggleUrl = "http://potter.genetics.utah.edu:8080/";
-var scoreField = "size";
+var scoreField = "";
 
 
 $(document).ready(function() {
@@ -15,9 +15,7 @@ $(document).ready(function() {
 	loadHeatmap("size");
 });
 
-
-function myJsonMethod(data) {
-	
+function myJsonMethod(data) {	
   	heatmap.score( function(d) { return d[scoreField]} );
 
   	var selection = d3.select("#chart").datum(data);
@@ -27,14 +25,16 @@ function myJsonMethod(data) {
 function loadHeatmap() {
 
 	scoreField = $("input:radio[name ='radio-value-field']:checked").val();
-	var url = giggleUrl + "?callback=JSON_CALLBACK&region=" + $('#overlaps').val();
+	var url = giggleUrl + "?region=" + $('#overlaps').val();
+
 
 	$.ajax({
 	    url: url,
-	    jsonp: "onGiggleData",
+	    jsonp: 'callback',
 	    type: "GET",
 	    dataType: "jsonp"
 	});
+
 
 //	d3.json("giggle.json", function(error, data) {
 //	});
